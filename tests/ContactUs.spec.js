be93ecf4c3dcc84db253771d47dccc8858d70f04
Bharath Reddy <bharathreddy2401@gmail.com>
2026-05-28 15:58:52 +0530

    Tested the functionality of contactus page

--- tests/ContactUs.spec.js
+++ tests/ContactUs.spec.js
@@ -8,16 +8,16 @@ test.describe("Testing the Contact us Functionality",()=>{
             await contact.navigate();
     });
 
-    test.skip("Testing the visibility of contact us button in the home page ",async({page})=>{
+    test("Testing the visibility of contact us button in the home page ",async({page})=>{
         await expect(page.getByRole("link",{name:"Contact us"})).toBeVisible();
     })
 
-    test.skip("Testing clicking the contact us button direct to contact us page",async({page})=>{
+    test("Testing clicking the contact us button direct to contact us page",async({page})=>{
         await contact.contactus();
         await expect(page).toHaveURL("https://automationexercise.com/contact_us");
     })
 
-    test.skip("Testing the Visibility of Get in touch form after clicking the contact us button",async({page})=>{
+    test("Testing the Visibility of Get in touch form after clicking the contact us button",async({page})=>{
         await contact.contactus();
         await expect(page.getByRole("heading",{name:"Get In Touch"})).toBeVisible();
     })
@@ -36,6 +36,18 @@ test.describe("Testing the Contact us Functionality",()=>{
        
     });
 
+    test("Testing clicking on the Home button after submitting the form redirects to home",async({page})=>{
+         await contact.contactus();
+        await page.getByPlaceholder("Name").fill("User12345");
+        await page.getByPlaceholder("Email").first().fill("User12345@gmail.com");
+        await page.getByPlaceholder("Subject").fill("Greetings");
+        await page.getByPlaceholder("Your Message Here").fill("Hello Good Morning");
+        page.on('dialog',async dialog =>{
+            await dialog.accept();
+        })
+        await page.locator('input[name="submit"]').click();
+        await page.getByRole("link",{name:"Home"}).nth(1).click();
+        await expect(page).toHaveURL("https://automationexercise.com/");
     
-
+    });
 });
