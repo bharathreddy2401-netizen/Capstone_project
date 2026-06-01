72615c736cccd2fe74318f05aff7e1459f949c22
Bharath Reddy <bharathreddy2401@gmail.com>
2026-06-01 15:06:08 +0530

    Added Account deletion functionality

---
+++ POM/deletion.js
@@ -0,0 +1,65 @@
+export class Delete {
+    constructor(page) {
+        this.page = page;
+        
+        // Login Locators
+        this.email1 = page.getByPlaceholder("Email Address").first();
+        this.password1 = page.getByPlaceholder("Password");
+        this.loginbtn = page.getByRole("button", { name: "Login" });
+
+        // Signup Locators
+        this.name = page.getByPlaceholder("Name");
+        this.email = page.getByPlaceholder("Email Address").nth(1);
+        this.btn = page.getByRole('button', { name: "Signup" });
+
+        // Account Creation Form Locators
+        this.password = page.locator("#password");
+        this.first_name = page.locator("#first_name");
+        this.last_name = page.locator("#last_name");
+        this.company = page.locator("#company");
+        this.address1 = page.locator("#address1");
+        this.address2 = page.locator("#address2");
+        this.country = page.locator("#country");
+        this.state = page.locator("#state");
+        this.city = page.locator("#city");
+        this.zipcode = page.locator("#zipcode");
+        this.mobile = page.locator("#mobile_number");
+        this.createAccountBtn = page.getByRole('button', { name: "Create Account" });
+        this.continue = page.getByRole("link",{name:"Continue"});
+    }
+
+    async navigate() {
+        await this.page.goto("https://automationexercise.com/"); 
+    }
+
+    async login() {
+        await this.email1.fill("bh1@1");
+        await this.password1.fill("1");
+        await this.loginbtn.click();
+    }
+
+    async signup() {
+        await this.name.fill("bh");
+        await this.email.fill("bh1@1");
+        await this.btn.click();
+    }
+
+    async createAccount() {
+        await this.page.getByRole("link", { name: "Signup / Login" }).click();
+        await this.signup();
+        
+        await this.password.fill("1");
+        await this.first_name.fill("bh");
+        await this.last_name.fill("Re");
+        await this.company.fill("abc");
+        await this.address1.fill("12-11");
+        await this.address2.fill("Delhi");
+        await this.country.selectOption("India");
+        await this.state.fill("Delhi");
+        await this.city.fill("Delhi");
+        await this.zipcode.fill("1234");
+        await this.mobile.fill("789456123");
+        await this.createAccountBtn.click();
+        await this.continue.click();
+    }
+}
---
+++ tests/Deletion.spec.js
@@ -0,0 +1,63 @@
+import {test,expect} from "@playwright/test";
+import {Delete} from "../POM/deletion";
+
+test.describe("Testing the Account Delete Functionality",()=>{
+    let delete1;
+    test.beforeEach(async ({page})=>{
+            delete1=new Delete(page);
+            await delete1.navigate();
+    });
+
+    test("Testing the non visibilty of account delete button in guest mode",async({page})=>{
+         await expect(page.getByText("Delete Account")).not.toBeVisible();
+    });
+
+    test("Testing the visibility of Delete Account button in the home page",async({page})=>{
+        await page.getByRole("link",{name:"Signup / Login"}).click();
+        await delete1.login();
+
+        
+        await expect(page.getByRole("link",{name:"Delete Account"})).toBeVisible();
+    });
+
+    test("Testing the clicking on account delete button deletes our account and displays account deleted",async({page})=>{
+        await delete1.createAccount();
+        await page.getByRole("link",{name:"Delete Account"}).click();
+        await expect(page.getByRole("heading",{name:"Account Deleted"})).toBeVisible();
+        //await delete1.createAccount();
+    });
+
+    test("testing the continue button click",async({page})=>{
+        await delete1.createAccount();
+        await page.getByRole("link",{name:"Delete Account"}).click();
+        await page.getByRole("link",{name:"Continue"}).click();
+        await expect(page.getByText("Category")).toBeVisible();
+    });
+
+    test("testing the UI header changes upon successful account deletion",async({page})=>{
+        await delete1.createAccount();
+        await expect(page.getByText("Logged in as bh")).toBeVisible();
+        await page.getByRole("link", { name: "Delete Account" }).click();
+        await expect(page.getByText("Logged in as bh")).not.toBeVisible();
+        await expect(page.getByRole("link", { name: "Logout" })).not.toBeVisible();
+    });
+
+    test("testing the data clearing by immediate re registration of the deleted email address",async({page})=>{
+        await delete1.createAccount();
+        await page.getByRole("link", { name: "Delete Account" }).click();
+        await page.getByRole("link", { name: "Continue" }).click();
+        await page.getByRole("link", { name: "Signup / Login" }).click();
+        await delete1.signup();
+        await expect(page.getByText("Enter Account Information")).toBeVisible();
+    });
+
+    test("Testing the account to be re created with the same email after a successful account deletion",async({page})=>{
+        await delete1.createAccount();
+        await page.getByRole("link", { name: "Delete Account" }).click();
+        await expect(page.getByRole("heading", { name: "Account Deleted" })).toBeVisible();
+        await page.getByRole("link", { name: "Continue" }).click();
+        await page.getByRole("link", { name: "Signup / Login" }).click();
+        await delete1.signup();
+        await expect(page.locator("#password")).toBeVisible();
+    });
+});
