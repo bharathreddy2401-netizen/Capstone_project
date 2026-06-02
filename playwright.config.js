faf9abb8549d0719ea341d66107b5c8bfc493fdf
Bharath Reddy <bharathreddy2401@gmail.com>
2026-06-02 13:42:23 +0530

    Updated the Interface Functionality

--- playwright.config.js
+++ playwright.config.js
@@ -16,11 +16,9 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   timeout:60000,
-
   expect:{
-    timeout: 30000,
+    timeout: 10000,
   },
- 
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
