4c17135fedf7cb60f1e18040625f9a2b936e9930
Bharath Reddy <bharathreddy2401@gmail.com>
2026-06-04 10:18:57 +0530

    updated the yml file for github

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
@@ -41,4 +41,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Express dashboard server running on port ${PORT}`);
-});
+});*/
